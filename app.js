a923e2f21a3c500b561bce956178680ee6a89981
Shubham Singh <shubham2101@gmail.com>
2023-10-19 00:14:38 +0530

    handling post req

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -13,13 +14,34 @@ const app = express();
 //     res.send("You can post to the endpoint..")
 // })
 
-const tours = JSON.parse(
+let tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  const newTour = { id: newId, ...req.body };
+  tours = [...tours, newTour];
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;

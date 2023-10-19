dd7fc76ba549dbe44bbbc958823d56f58f05e5ef
Shubham Singh <shubham2101@gmail.com>
2023-10-20 00:03:22 +0530

    separate app and server file

--- app.js
+++ app.js
@@ -15,32 +15,11 @@ app.use(express.json());
 //     res.send("You can post to the endpoint..")
 // })
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 // app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
-// app
-//   .route('/api/v1/tours/:id')
-//   .get(getTour)
-//   .patch(updateTour)
-//   .delete(deleteTour);
+module.exports = app
 
-// app.route('/api/v1/users').get(getAllUsers).post(createUser);
 
-// app
-//   .route('/api/v1/users/:id')
-//   .get(getUser)
-//   .patch(updateUser)
-//   .delete(deleteUser);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`app is running on the ${port}...`);
-});

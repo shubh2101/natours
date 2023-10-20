4414d7ae7855231d57b022e2493fb5e0ba1b7edd
Shubham Singh <shubham2101@gmail.com>
2023-10-20 19:09:53 +0530

    connecting to mongoDB atlas

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+async function main() {
+  const db = process.env.DB_URI.replace('<password>', process.env.DB_PASSWORD);
+  await mongoose
+    .connect(db)
+    .then(() => console.log('DB connected succesfully.'))
+    .catch((err) => console.log(err));
+}
+
+main()
+
 const port = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV);
 
 app.listen(port, () => {
   console.log(`server is running on the ${port}...`);

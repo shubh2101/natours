0d3b6c2eba6c11e021e3fea416ccb2ecc61644c1
Shubham Singh <shubham2101@gmail.com>
2023-10-22 21:16:41 +0530

    refactoring APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,50 +10,54 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    let queryStr = JSON.stringify(queryObj);
-    //advance filtering
-    queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-    let query = Tour.find(JSON.parse(queryStr));
+    // const queryObj = { ...req.query };
+    // let queryStr = JSON.stringify(queryObj);
+    // //advance filtering
+    // queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
     //sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt _id');
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort('-createdAt _id');
+    // }
 
     //limiting fileds
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // }
 
     //pagination
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    const totalTours = await Tour.countDocuments();
-    if (req.query.page) {
-      if (skip >= totalTours) throw new Error('This page does not exist');
-    }
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = parseInt(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+    // const totalTours = await Tour.countDocuments();
+    // if (req.query.page) {
+    //   if (skip >= totalTours) throw new Error('This page does not exist');
+    // }
 
-    const totalPages = Math.ceil(totalTours / limit);
+    // const totalPages = Math.ceil(totalTours / limit);
 
     //execute query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
       count: tours.length,
-      totalTours,
-      currentPage: page,
-      totalPages,
+      // currentPage: page,
       data: { tours },
     });
   } catch (err) {
+    console.log(err);
     res.status(404).json({
       status: 'Failed',
       message: err,
---
+++ utils/apiFeatures.js
@@ -0,0 +1,41 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, (match) => `$${match}`);    
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt _id');
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    }
+    return this;
+  }
+  paginate() {
+    const page = parseInt(this.queryString.page) || 1;
+    const limit = parseInt(this.queryString.limit) || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+module.exports = APIFeatures;

e05076d7a160d30f445846db2bd8455284e2921e
Shubham Singh <shubham2101@gmail.com>
2023-10-24 23:12:48 +0530

    aggregation grouping all doc in one

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,38 +10,6 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // const queryObj = { ...req.query };
-    // let queryStr = JSON.stringify(queryObj);
-    // //advance filtering
-    // queryStr = queryStr.replace(/\b(gte?|lte?)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-    // let query = Tour.find(JSON.parse(queryStr));
-    //sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort('-createdAt _id');
-    // }
-
-    //limiting fileds
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // }
-
-    //pagination
-    // const page = parseInt(req.query.page) || 1;
-    // const limit = parseInt(req.query.limit) || 10;
-    // const skip = (page - 1) * limit;
-    // query = query.skip(skip).limit(limit);
-    // const totalTours = await Tour.countDocuments();
-    // if (req.query.page) {
-    //   if (skip >= totalTours) throw new Error('This page does not exist');
-    // }
-
-    // const totalPages = Math.ceil(totalTours / limit);
-
     //execute query
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
@@ -57,7 +25,36 @@ exports.getAllTours = async (req, res) => {
       data: { tours },
     });
   } catch (err) {
-    console.log(err);
+    res.status(404).json({
+      status: 'Failed',
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
     res.status(404).json({
       status: 'Failed',
       message: err,

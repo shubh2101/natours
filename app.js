91444cd1a90f5ca3eaa121b0c2a39136ffcb11db
Shubham Singh <shubham2101@gmail.com>
2023-11-01 20:23:02 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.json());
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
-module.exports = app
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
-
+module.exports = app;

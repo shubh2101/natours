c3736ca8a94d95720ff42166c35f8637a252bd09
Shubham Singh <shubham2101@gmail.com>
2023-11-03 19:01:04 +0530

    Stop commiting congig.env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,40 @@
+const sendDevErrror = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendProdError = (err, res) => {
+  //operational trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //programming errors or other unknown error : don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong.',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  console.log(err)
+  console.log(process.env.NODE_ENV);
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendDevErrror(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdError(err, res);
+  }
 };

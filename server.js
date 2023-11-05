c20de3756cb9d5aa18e8610871f182beed812fb1
Shubham Singh <shubham2101@gmail.com>
2023-11-05 19:58:07 +0530

    unhandled rejections

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ async function main() {
   const db = process.env.DB_URI.replace('<password>', process.env.DB_PASSWORD);
   await mongoose
     .connect(db)
-    .then(() => console.log('DB connected succesfully.'))
-    .catch((err) => console.log(err));
+    .then(() => console.log('DB connected succesfully.'));
+  // .catch((err) => console.log(err));
 }
 
-main()
+main();
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is running on the ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+  server.close(() => {
+    process.exit(1);
+  });
 });

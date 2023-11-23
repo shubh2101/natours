c74f0323ed8dd7319ff8036c6fef01772470af86
Shubham Singh <shubham2101@gmail.com>
2023-11-23 17:37:18 +0530

    signing users with jwt sign

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,27 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRESIN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+//to generate random secret key
+//node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
